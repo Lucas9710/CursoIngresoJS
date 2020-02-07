e02f4a5a695b2616caffaa6d9cd2b05009f36e95
alumno <alumno@lab6pc00>
2020-02-07 11:50:14 -0300

    ejercicio 9

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -3,7 +3,16 @@ transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
-{
+{   
+    //guardo informacion en las variables
+    var sueldo = parseInt(document.getElementById("sueldo").value);
+    
+    //saco porcentajes y le sumo al sueldo
+    var aumento = sueldo * 10 / 100;
+    var sueldoFinal = sueldo + aumento;
 
-	
+    //agrego el resultado
+    document.getElementById("resultado").value = sueldoFinal;
+
+
 }
