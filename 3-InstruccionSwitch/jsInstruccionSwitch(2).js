e60ea104ac432298a6932da494ad8721b0ea2f87
alumno <alumno@LAB00PC00>
2020-02-14 10:28:50 -0300

    correcion ejercicio 2

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -5,46 +5,17 @@ var mes = document.getElementById('mes').value;
 
 switch(mes){
 
-  case "Enero": alert("Ya pasamos el frio, ahora calor!!!." );
-  break;
-
-  case "Febrero": alert("Ya pasamos el frio, ahora calor!!!." );
-  break;
-
-  case "Marzo": alert("Ya pasamos el frio, ahora calor!!!." );
-  break;
-
-
-  case "Abril": alert("Falta para el invierno." );
-  break;
-
-
-  case "Mayo": alert("Falta para el invierno." );
-  break;
-
-
+  case "Abril":
+  case "Mayo": 
   case "Junio": alert("Falta para el invierno." );
   break;
 
-
-  case "Julio": alert("Abrigate que hace frio." );
-  break;
-
+  case "Julio":
   case "Agosto": alert("Abrigate que hace frio.");
   break;
 
-  case "Septiembre": alert("Ya pasamos el frio, ahora calor!!!." );
-  break;
-
-  case "Octubre": alert("Ya pasamos el frio, ahora calor!!!." );
-  break;
-
-  case "Noviembre": alert("Ya pasamos el frio, ahora calor!!!." );
-  break;
-
-  case "Diciembre": alert("Ya pasamos el frio, ahora calor!!!." );
-  break;
-
+  default: alert("Ya pasamos el frio, ahora calor!!!.");
+  
 
     }
   
