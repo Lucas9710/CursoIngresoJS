b250c5ab33188b699ba1e08c21f9823cefe543c7
alumno <alumno@LAB00PC00>
2020-02-14 10:31:03 -0300

    ejercicio 2 identado

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -7,17 +7,19 @@ switch(mes){
 
   case "Abril":
   case "Mayo": 
-  case "Junio": alert("Falta para el invierno." );
-  break;
+  case "Junio": 
+        alert("Falta para el invierno." );
+        break;
 
   case "Julio":
-  case "Agosto": alert("Abrigate que hace frio.");
-  break;
+  case "Agosto": 
+        alert("Abrigate que hace frio.");
+        break;
 
-  default: alert("Ya pasamos el frio, ahora calor!!!.");
+  default:
+        alert("Ya pasamos el frio, ahora calor!!!.");
   
-
-    }
+  }
   
 
 }//FIN DE LA FUNCIÓN
